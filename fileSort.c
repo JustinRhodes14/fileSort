b8ec00f4aef725790dac7e0a13606f68a60c3972
Justin <rhodesju21@gmail.com>
2020-02-19 00:48:47 -0500

    now properly tokenizes all strings, need to fix linked list functionality, keeps copying same word in for some reason?

--- fileSort.c
+++ fileSort.c
@@ -7,12 +7,17 @@
 #include <unistd.h>
 #include <string.h>
 
+enum boolean {true = 1, false = 0};
+
 typedef struct _node {
 	void *data;
 	struct _node *next;
 }Node;
 
 void LLIterator(Node*);
+void freeLL(Node*);
+enum boolean commaDetect(char);
+enum boolean badChar(char);
 
 int main(int argc, char** argv) {
 
@@ -26,34 +31,35 @@ int main(int argc, char** argv) {
 	int fd = open("test.txt",O_RDONLY);	
 	
 	Node *head = (Node*)malloc(sizeof(Node));
-
-	head->data = "hello"; 
-	head->next = NULL;
-	printf("val of head: %s\n",head->data);	
-	
-	char buffer[101];
-	memset(buffer,'\0',101);	
+	head->data = "first";
+	head->next = NULL; 
 	int bytesRead = 1;
+	int count = 0;
 	int curr = 0;
+	char* last = ",";
+	char word[257]; 
 	do {
-		
-		if (curr >= 100) {
-			curr = 0;
+		char buffer;
+		bytesRead = read(fd,&buffer,1);
+		if (commaDetect(buffer) == true){
 			Node *temp = (Node*)malloc(sizeof(Node));
-			temp->data = buffer;
+			temp->data = word;
 			temp->next = head->next;
 			head->next = temp;
-			memset(buffer,'\0',101);	
+			printf("val of word: %s\n",word);
+			memset(word,'\0',257);
+			curr = 0;
+		}else if(badChar(buffer) == true) {
+			continue;
+		}  else {
+			word[curr] = buffer;
+			curr++;
 		}
-		bytesRead = read(fd,buffer,100-curr);
-		curr += bytesRead;		
+		count+= bytesRead;
 	}while(bytesRead > 0);
-	
-	close(fd);	
-	LLIterator(head);
-	printf("buffer bytes: %d\n",bytesRead);
-	printf("buffer: %s\n",buffer);
-	printf("type: %c\n", type);
+	printf("val of word: %s\n", word);
+	//LLIterator(head);
+	close(fd);
 	return 0;
 
 
@@ -61,16 +67,36 @@ int main(int argc, char** argv) {
 
 void LLIterator (Node *ptr) {
 	while(ptr != NULL) {
-		printf("LNode: %s->",ptr->data);
+		printf(" LNode: %s->",ptr->data);
 		ptr = ptr->next;
 	}
 	printf("\n");
 }
 
-int badChar(char buffer) {
-	if(buffer == '\t' || buffer == '_' || buffer == '\n' || buffer == ' ') {
-		return 1;
+void freeLL(Node *ptr) {
+
+	while(ptr != NULL) {
+		Node* temp = ptr->next;
+		Node* temp2 = ptr;
+		ptr = temp;
+		free(temp2);
+	}
+
+}
+
+enum boolean badChar(char input) {
+	if(input == '\t' || input == '_' || input == '\n' || input == ' ') {
+		return true;
 	}	
 
-	return 0;
+	return false;
+}
+
+enum boolean commaDetect(char input) {
+	if (input == ',') {
+		return true;
+	}
+
+	return false;
+
 }
